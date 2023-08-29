0b0690f613a8d92bef7efba5fec07942a07207ea
Sunil-Pradhan <pradhan.sunil@outlook.com>
2023-08-29 23:45:25 +0530

    React crud app

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ContactCard from './ContactCard';
+
+const ContactList = (props) => {
+  console.log(props);
+
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      // <div className="item">
+      //   <div className="content">
+      //     <div className="header">{contact.name}</div>
+      //     <div>{contact.email}</div>
+      //     <i className="trash alternate outline icon"></i>
+      //   </div>
+      // </div>
+      <ContactCard
+        contact={contact}
+        clickHandler={deleteContactHandler}
+        key={contact.id}
+      ></ContactCard>
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+};
+
+export default ContactList;
